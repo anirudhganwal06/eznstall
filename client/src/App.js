da57d23dfdc774252269e662cc4eab633667e2a9
Anirudh <anirudhganwal06@gmail.com>
2020-05-28 17:28:48 +0530

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
+import Header from "./components/Layout/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main></main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
